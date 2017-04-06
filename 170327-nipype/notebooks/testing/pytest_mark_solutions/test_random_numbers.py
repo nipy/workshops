590441c2d7fc69f5c5c411496949f95633883696
Dorota Jarecka <djarecka@gmail.com>
2017-04-06 08:36:47 -0400

    add xfail

--- 170327-nipype/notebooks/testing/pytest_mark_solutions/test_random_numbers.py
+++ 170327-nipype/notebooks/testing/pytest_mark_solutions/test_random_numbers.py
@@ -1,8 +1,10 @@
 import random
 import numpy as np
 from random_numbers import mean_random_numbers
+import pytest, sys
 
-
+@pytest.mark.xfail(sys.version_info < (3,0),
+                   reason="random api is different in python2")
 def test_random_numbers():
     random.seed(3)
     np.testing.assert_almost_equal(mean_random_numbers(), 47)
